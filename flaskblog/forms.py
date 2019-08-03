e012b52fe5ea03a79592babf60271f6503eca848
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-04 00:09:44 +0300

    add customized validation functions in models to avoid using same emails and username

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, DataRequired,Email,EqualTo, Length
+from wtforms.validators import DataRequired, DataRequired,Email,EqualTo, ValidationError, Length
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-
+from flaskblog.models import User
 class RegistrationForm (FlaskForm):
 	username = StringField('Enter Username', validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email', validators=[DataRequired(), Email()])
@@ -9,6 +9,15 @@ class RegistrationForm (FlaskForm):
 	confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password',message='passwords must match')])
 	submit = SubmitField('Sign Up')
 
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user:
+			raise ValidationError('That username is takes, please use another')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email = email.data).first()
+		if user:
+			raise ValidationError('That email is takes, please use another')
 class LoginForm (FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators = [DataRequired()])
