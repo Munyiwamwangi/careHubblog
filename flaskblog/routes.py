606b236ad062fe94425f04f43ad1333af533890f
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-04 17:23:12 +0300

    adding profile picture upload and save

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,5 @@
+import secrets
+import os
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db , bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -72,12 +74,20 @@ def logout():
 	logout_user()
 	return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	f_name,f_ext = os.path.splitext(form_picture.filename)
+
+
 #ACCOUNT
 @app.route("/account", methods=['GET','POST'])
 @login_required
 def account():
 	form = UpdateAccountForm()
 	if form.validate_on_submit():
+		if form.picture.data:
+
 		current_user.username = form.username.data
 		current_user.email = form.email.data
 		db.session.commit()
@@ -86,5 +96,6 @@ def account():
 	elif request.method == 'GET':
 		form.username.data = current_user.username
 		form.email.data = current_user.email
-		image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-		return render_template('account.html', title = 'Account', image_file = image_file, form=form)
+	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+	return render_template('account.html', title = 'Account', 
+		image_file = image_file, form=form)
