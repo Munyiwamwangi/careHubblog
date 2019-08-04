bb65636b5fc48793940803a8def39b7a8fa8ba11
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-04 09:16:35 +0300

    tied users to login so that they can only access user account infof if they are logged in

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -15,4 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+#we want to tie users to login so that they can only access user account infof if they are logged in
+login_manager.login_view = 'Login'
 from flaskblog import routes
