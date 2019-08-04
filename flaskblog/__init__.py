e55a0165d8785bbb33eb112cdce990829a47a969
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-04 09:50:14 +0300

    user profile enabling image loadinf from static folder prifile_pics directory

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,10 +11,11 @@ app.config['SECRET_KEY'] = 'de74e7b0fad76d362bfd1fcd0c0fd885'
 #init db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #db instance
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 #we want to tie users to login so that they can only access user account infof if they are logged in
-login_manager.login_view = 'Login'
+login_manager.login_view = 'login'
+#login_message_category is a bootstrap class
+login_manager.login_message_category = 'info'
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db , bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
@@ -57,7 +57,10 @@ def login():
 		user = User.query.filter_by(email = form.email.data).first()
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
 			login_user(user, remember = form.remember.data)
-			return redirect(url_for('home'))
+			next_page = request.args.get('next')
+			#redirect to the next page if it exists, else render home
+			#if there is not a next page, always render home
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash(f'Login Unsuccesfull. Please check email and password.', 'danger')
 	return render_template('login.html', title = 'Login', form = form)
@@ -73,4 +76,5 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-	return render_template('account.html', title = 'Account')
+	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+	return render_template('account.html', title = 'Account', image_file = image_file)
