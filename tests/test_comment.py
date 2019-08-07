4728a262acdb7735446f5d0565317a0b0c3f79d8
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-07 08:22:08 +0300

    update tests

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,2 +1,18 @@
 import unittest
 from flaskblog import Comment
+from flask import current_app
+from flaskblog import create_app
+
+#this is a basic test manager
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('development')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+# prevents default faults such as unintended addition of info into the db
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
--- tests/test_post.py
+++ tests/test_post.py
@@ -1,2 +1,14 @@
 import unittest
 from flaskblog import Post
+
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_post = Post(password = 'password')
+
+    def test_login_scope(self):
+            with self.assertRaises(AttributeError):
+                self.new_post.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_post.verify_password('password'))
